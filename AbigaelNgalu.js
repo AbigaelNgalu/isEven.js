037657b25a95487bafd71214b75455c86c9602b8
Abigael Ngalu <abigaelngalu@gmail.com>
2025-09-25 13:53:24 +0000

    update the changes

---
+++ AbigaelNgalu.js
@@ -0,0 +1,11 @@
+// isEven.js
+function isEven (num){
+   if (num % 2===0)
+    return true;
+}
+console.log(isEven(4)); // true
+console.log(isEven(7));// false
+console.log(isEven(2)); // true
+
+
+
